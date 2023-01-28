14778e15d7e6fe4978ec45664fb2584fd81bb974
Your Name <you@example.com>
2023-01-28 15:59:17 -0300

    criando função exibeCirculo

--- script.js
+++ script.js
@@ -5,10 +5,17 @@ let pincel = tela.getContext('2d');
 pincel.fillStyle = 'grey';
 pincel.fillRect(0,0,600,400);
 
-function exibeAlerta(event) {
-    alert("Cliquei");
+function exibeCirculo(event) {
+    let x = event.clientX - tela.offsetLeft;  // Pegar coordenadas da posição do click
+    let y = event.clientY - tela.offsetTop;  //Tirar a borda branca que fica de fora do canvas pq ele conta com ela.
+
+    pincel.fillStyle = 'red';
+    pincel.beginPath();
+    pincel.arc(x,y, 10, 0, 2 * Math.PI);
+    pincel.fill();
+
     console.log(event);
 }
 
 
-tela.onclick = exibeAlerta;
+tela.onclick = exibeCirculo;
