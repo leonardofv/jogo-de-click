ed5f3b9c70220152e47276a7dc27c750f8e50352
Your Name <you@example.com>
2023-02-02 14:55:29 -0300

    atualizei a tela para o alvo mudar sua posição aleatóriamente

--- script.js
+++ script.js
@@ -8,15 +8,27 @@ pincel.fillRect(0,0,600,400);
 let raio = 10;
 
 function exibeCirculo(x, y, raio, cor) {
-    
+
     pincel.fillStyle = cor;
     pincel.beginPath();
     pincel.arc(x,y, raio, 0, 2 * Math.PI);
     pincel.fill();
-
-    console.log(event);
 }
 
-exibeCirculo(300, 200, raio+20, 'red');
-exibeCirculo(300, 200, raio+10, 'white');
-exibeCirculo(300, 200, raio, 'red');
+function exibeAlvo(xAleatorio, yAleatorio) {
+
+    exibeCirculo(xAleatorio, yAleatorio, raio+20, 'red');
+    exibeCirculo(xAleatorio, yAleatorio, raio+10, 'white');
+    exibeCirculo(xAleatorio, yAleatorio, raio, 'red');
+}
+
+function atualizaTela() {
+    pincel.clearRect(0, 0, 600, 400);  //Limpar a tela após o alvo aparecer
+    //Sortear posição aleatoria do x e y.
+    let xAleatorio = Math.round(Math.random() * 600);
+    let yAleatorio = Math.round(Math.random() * 400);
+
+    exibeAlvo(xAleatorio, yAleatorio);
+}
+
+setInterval(atualizaTela, 1000);
