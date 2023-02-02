fb9449361ed1dd79453d704ffe4976eb6f8ea31b
Your Name <you@example.com>
2023-02-02 14:15:05 -0300

    alterando novamente o código para exibir o circulo

--- script.js
+++ script.js
@@ -5,17 +5,18 @@ let pincel = tela.getContext('2d');
 pincel.fillStyle = 'grey';
 pincel.fillRect(0,0,600,400);
 
-function exibeCirculo(event) {
-    let x = event.clientX - tela.offsetLeft;  // Pegar coordenadas da posição do click
-    let y = event.clientY - tela.offsetTop;  //Tirar a borda branca que fica de fora do canvas pq ele conta com ela.
+let raio = 10;
 
-    pincel.fillStyle = 'red';
+function exibeCirculo(x, y, raio, cor) {
+    
+    pincel.fillStyle = cor;
     pincel.beginPath();
-    pincel.arc(x,y, 10, 0, 2 * Math.PI);
+    pincel.arc(x,y, raio, 0, 2 * Math.PI);
     pincel.fill();
 
     console.log(event);
 }
 
-
-tela.onclick = exibeCirculo;
+exibeCirculo(300, 200, raio+20, 'red');
+exibeCirculo(300, 200, raio+10, 'white');
+exibeCirculo(300, 200, raio, 'red');
